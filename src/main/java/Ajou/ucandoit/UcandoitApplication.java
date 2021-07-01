c66280a3afdf5cef1ab3a305d336c09edce309fe
hajin <gkwls1130@ajou.ac.kr>
2021-07-02 00:09:45 +0900

    [byeon] set Develop Environment

---
+++ src/main/java/Ajou/ucandoit/UcandoitApplication.java
@@ -0,0 +1,13 @@
+package Ajou.ucandoit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UcandoitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UcandoitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Ajou/ucandoit/UcandoitApplicationTests.java
@@ -0,0 +1,13 @@
+package Ajou.ucandoit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UcandoitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

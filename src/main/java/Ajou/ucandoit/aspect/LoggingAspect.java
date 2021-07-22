84884f3bbf853ec2b539999641abd6bd2c652186
HYOTAEK <hyotaek9812@gmail.com>
2021-07-22 16:05:18 +0900

    travis test

--- src/main/java/Ajou/ucandoit/aspect/LoggingAspect.java
+++ src/main/java/Ajou/ucandoit/aspect/LoggingAspect.java
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Component;
 @Aspect
 @Component
 public class LoggingAspect {
-
+//
 //    @Before(("execution(* Ajou.ucandoit.service.*.get*(..))")) // 서비스 패키지에 있는 어떤 클래스든 get으로 시작하고 파라미터는 상관없다!
 //    public void loggerBefore() {
 //        System.out.println("get으로 시작되는 메서드 시작");
